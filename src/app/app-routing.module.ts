3dfcd415d14202ab17cde2caf09d7f2b6118ebf6
Pooja P <poojaprabhu567@gmail.com>
2021-04-21 18:12:13 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+
+import { CardsComponent } from './cards/cards.component';
+import { CarouselComponent } from './carousel/carousel.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component: CarouselComponent},
+  
+  {path:'projects',component: CardsComponent},
+  
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import {MatToolbarModule,MatButtonModule,MatIconModule} from '@angular/material';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselComponent } from './carousel/carousel.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';  
+
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { CardsComponent } from './cards/cards.component';
+
+import { RouterModule } from '@angular/router';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    CardsComponent,
+    
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    CarouselModule ,
+    RouterModule,
+  
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
